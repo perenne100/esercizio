68790c10d2ce697a2c6e1c2bf82fa7ba5ac7f757
perenne100 <148433978+perenne100@users.noreply.github.com>
2023-12-30 19:31:27 +0100

    Create esercizi2.js

---
+++ esercizi2.js
@@ -0,0 +1,23 @@
+/*1) Modificare l'esercizio libreria affinché la libreria venga visualizzata su una tabella,
+dove ogni riga corrisponde ad un libro.
+
+Ogni libro dovrà avere le seguenti informazioni:
+1. nome (text)
+2. autore (text)
+3. Data di uscita del libro (non anno)
+4. Numero di pagine totali del libro (integer)
+
+RICERCA: sopra la tabella deve essere presente un input type text ed un bottone "cerca".
+Al click del bottone devono essere cercati tutti i libri dell'autore scritto sulla text. 
+Le righe dei libri trovati devono essere colorate di verde (potete ridisegnare la tabella). 
+Attenzione, il nome dell'autore potrebbe essere incompleto, ad esempio ricerca per nome e/o cognome
+
+INSERIMENTO: creare una form per inserire un nuovo libro. 
+1. controllare che tutti i campi siano stati inseriti e che abbiano dei valori corretti.
+2. Il nuovo libro correttamente inserito deve essere visualizzato sulla tabella 
+(potete ridisegnare la tabella).
+
+CANCELLA: creare una form per eliminare un libro.
+1. Il libro può essere cancellato solo se vengono selezionati il nome e l'autore del libro.
+2. Se uno dei due campi non è settato allora deve apparire un alert di avviso (potete ridisegnare la tabella).*/
+    
